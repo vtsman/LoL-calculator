ab522c2cebb0f927157df3b0f5b4f1b6e39e4cd5
vtsman <hokiespencer@gmail.com>
2015-07-12 16:09:15 -0400

    Moved to Riot API wrapper, added support for every champion, stats table implemented, stats table reacts to level

--- champion.js
+++ champion.js
@@ -1,31 +1,97 @@
 /**
  * Created by Spencer on 7/11/15.
  */
-angular.module('build', [])
-    .controller('ChampionController', function($scope) {
-        var champion = this;
-        current_champion = undefined;
-        $.getJSON("./content/json/champions/teemo.json").done(
-            function(data){
-                current_champion = data;
-                apply();
-            }
-        );
+api_url="http://localhost:5000/"
 
-        champion.should_show = function(){
-            return current_champion != undefined
+app = angular.module('build', []);
+app.controller('ChampionController', function($scope) {
+    var champion = this;
+    var current_champion = undefined;
+    $.ajax({
+        method: "GET",
+        url: api_url + "api/lol/static-data/na/v1.2/champion/17",
+        data: { "champData": "all" },
+        async: 'true'
+    }).done(
+        function(data){
+            current_champion = JSON.parse(data);
+            console.log(current_champion)
+            apply();
         }
+    ).fail(function(error){
+            console.log(error)
+        });
 
-        champion.getChampion = function(){
-            return current_champion;
-        }
+    champion.should_show = function(){
+        return current_champion != undefined
+    }
 
-        apply = function(){
-            $scope.$apply();
+    champion.getChampion = function(){
+        return current_champion;
+    }
 
-            var icon = $('#champion_icon');
-            var text = $('#champion_header_text');
+    champion.getLevel = function(){
+        return $("#champion_level").val()
+    }
 
-            icon.height(text.height());
-        }
-    });
+    champion.getArmor = function(){
+        return champion.getChampion().stats.armor + (champion.getChampion().stats.armorperlevel * champion.getLevel())
+    }
+
+    champion.getAD = function () {
+        return champion.getChampion().stats.attackdamage + (champion.getChampion().stats.attackdamageperlevel * champion.getLevel())
+    }
+
+    champion.getRange = function() {
+        return champion.getChampion().stats.attackrange
+    }
+
+    champion.getAttackSpeed = function(){
+        return Math.round(((0.625 / (1 + champion.getChampion().stats.attackspeedoffset)) * (1 + champion.getChampion().stats.attackspeedperlevel * champion.getLevel() / 100)) * 1000) / 1000
+    }
+
+    champion.getCritChance = function(){
+        return champion.getChampion().stats.crit + (champion.getChampion().stats.critperlevel * champion.getLevel())
+    }
+
+    champion.getHP = function(){
+        return champion.getChampion().stats.hp + (champion.getChampion().stats.hpperlevel * champion.getLevel())
+    }
+
+    champion.getHPRegen = function(){
+        return champion.getChampion().stats.hpregen + (champion.getChampion().stats.hpregenperlevel * champion.getLevel())
+    }
+
+    champion.getSpeed = function(){
+        return champion.getChampion().stats.movespeed
+    }
+
+    champion.getMana = function(){
+        return champion.getChampion().stats.mp + (champion.getChampion().stats.mpperlevel * champion.getLevel())
+    }
+
+    champion.getManaRegen = function(){
+        return champion.getChampion().stats.mpregen + (champion.getChampion().stats.mpregenperlevel * champion.getLevel())
+    }
+
+    champion.getMR = function(){
+        return champion.getChampion().stats.spellblock + (champion.getChampion().stats.spellblockperlevel * champion.getLevel())
+    }
+
+    apply = function(){
+        $scope.$apply();
+
+        var icon = $('#champion_icon');
+        var text = $('#champion_header_text');
+
+        icon.height(text.height());
+    }
+});
+app.filter('range', function() {
+    return function(input, total) {
+        total = parseInt(total);
+        for (var i=0; i<total; i++)
+            input.push(i);
+        return input;
+    };
+});
