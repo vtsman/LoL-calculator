d77301c736c1d27670f120061cf50ca74b001a05
vtsman <hokiespencer@gmail.com>
2015-07-15 23:21:18 -0400

    Items are now implemented to a point that they can add static stats

--- champion.js
+++ champion.js
@@ -1,3 +1,277 @@
 /**
  * Created by Spencer on 7/14/15.
  */
+var Champion = function(riot_json, custom_json){
+    this.level = 0;
+    this.base = riot_json;
+
+    this.add = {};
+    this.mult = {};
+
+    this.state = {
+        cooldowns: [0, 0, 0, 0],
+        buffs: [],
+        items: [0, 0, 0, 0, 0, 0],
+        levels: [0, 0, 0, 0]
+    }
+
+    this.resetStats()
+}
+
+Champion.prototype.resetStats = function () {
+    this.add.armor = 0;
+    this.mult.armor = 1;
+
+    this.add.ad = 0;
+    this.mult.ad = 1;
+
+    this.add.range = 0;
+    this.mult.range = 1;
+
+    this.add.attackSpeed = 0;
+    this.mult.attackSpeed = 1;
+
+    this.add.crit = 0;
+    this.mult.crit = 1;
+
+    this.add.hp = 0;
+    this.mult.hp = 1;
+
+    this.add.hpRegen = 0;
+    this.mult.hpRegen = 1;
+
+    this.add.speed = 0;
+    this.mult.speed = 1;
+
+    this.add.mana = 0;
+    this.mult.mana = 1;
+
+    this.add.manaRegen = 0;
+    this.mult.manaRegen = 1;
+
+    this.add.mr = 0;
+    this.mult.mr = 1;
+
+    this.add.ap = 0;
+    this.mult.ap = 1;
+
+    this.cdr = [0];
+}
+
+Champion.prototype.calculateItems = function(){
+    this.resetStats()
+    var self = this;
+    this.getItems().forEach(function(id){
+        if(id == 0){
+            return;
+        }
+        var item = items.data[id]
+        if(item.stats == undefined){
+            return;
+        }
+        for(var key in item.stats){
+            switch(key){
+                case "FlatArmorMod": self.add.armor += item.stats[key]; break;
+                case "FlatMovementSpeedMod": self.add.speed += item.stats[key]; break;
+                case "FlatHPPoolMod": self.add.hp += item.stats[key]; break;
+                case "FlatCritChanceMod": self.add.crit += item.stats[key]; break;
+                case "FlatMagicDamageMod": self.add.ap += item.stats[key]; break;
+                case "FlatMPPoolMod": self.add.mana += item.stats[key]; break;
+                case "FlatSpellBlockMod": self.add.mr += item.stats[key]; break;
+                case "FlatPhysicalDamageMod": self.add.ad += item.stats[key]; break;
+                case "PercentAttackSpeedMod": self.mult.attackSpeed *= 1 + item.stats[key]; break;
+                case "PercentLifeStealMod": break; /*FIXME Implement*/
+                case "FlatHPRegenMod": self.add.hpRegen += item.stats[key]; break;
+                case "FlatMPRegenMod": self.add.manaRegen += item.stats[key]; break;
+                case "PercentMovementSpeedMod": self.mult.speed *= 1 + item.stats[key]; break
+
+                default: console.log(key); break;
+            }
+        }
+    })
+}
+
+Champion.prototype.getBaseArmor = function(){
+    return (this.base.stats.armor + (this.base.stats.armorperlevel * this.level))
+}
+
+Champion.prototype.getArmor = function(){
+    this.calculateItems();
+    return (this.getBaseArmor() + this.add.armor) * this.mult.armor
+}
+
+Champion.prototype.getBaseAD = function () {
+    return (this.base.stats.attackdamage + (this.base.stats.attackdamageperlevel * this.level))
+}
+
+Champion.prototype.getAD = function(){
+    this.calculateItems();
+    return (this.getBaseAD() + this.add.ad) * this.mult.ad
+}
+
+Champion.prototype.getBaseRange = function() {
+    return (this.base.stats.attackrange)
+}
+
+Champion.prototype.getRange = function(){
+    this.calculateItems();
+    return (this.getBaseRange() + this.add.range) * this.mult.range
+}
+
+Champion.prototype.getBaseAttackSpeed = function(){
+    return (((0.625 / (1 + this.base.stats.attackspeedoffset)) * (1 + this.base.stats.attackspeedperlevel * this.level / 100)))
+}
+
+Champion.prototype.getAttackSpeed = function(){
+    this.calculateItems();
+    return (this.getBaseAttackSpeed() + this.add.attackSpeed) * this.mult.attackSpeed
+}
+
+Champion.prototype.getBaseCritChance = function(){
+    return (this.base.stats.crit + (this.base.stats.critperlevel * this.level))
+}
+
+Champion.prototype.getCritChance = function(){
+    this.calculateItems();
+    return (this.getBaseCritChance() + this.add.crit) * this.mult.crit;
+}
+
+Champion.prototype.getBaseHP = function(){
+    return (this.base.stats.hp + (this.base.stats.hpperlevel * this.level))
+}
+
+Champion.prototype.getHP = function(){
+    this.calculateItems();
+    return (this.getBaseHP() + this.add.hp) * this.mult.hp
+}
+
+Champion.prototype.getBaseHPRegen = function(){
+    return (this.base.stats.hpregen + (this.base.stats.hpregenperlevel * this.level))
+}
+
+Champion.prototype.getHPRegen = function(){
+    this.calculateItems();
+    return (this.getBaseHPRegen() + this.add.hpRegen) * this.mult.hpRegen
+}
+
+Champion.prototype.getBaseSpeed = function(){
+    return (this.base.stats.movespeed)
+}
+
+Champion.prototype.getSpeed = function(){
+    this.calculateItems();
+    return (this.getBaseSpeed() + this.add.speed) * this.mult.speed;
+}
+
+Champion.prototype.getBaseMana = function(){
+    return (this.base.stats.mp + (this.base.stats.mpperlevel * this.level))
+}
+
+Champion.prototype.getMana = function(){
+    this.calculateItems();
+    return (this.getBaseMana() + this.add.mana) * this.mult.mana;
+}
+
+Champion.prototype.getBaseManaRegen = function(){
+    this.calculateItems();
+    return (this.base.stats.mpregen + (this.base.stats.mpregenperlevel * this.level))
+}
+
+Champion.prototype.getManaRegen = function(){
+    this.calculateItems();
+    return (this.getBaseManaRegen() + this.add.manaRegen) * this.mult.manaRegen
+}
+
+Champion.prototype.getBaseMR = function(){
+    return (this.base.stats.spellblock + (this.base.stats.spellblockperlevel * this.level))
+}
+
+Champion.prototype.getMR = function(){
+    this.calculateItems();
+    return (this.getBaseMR() + this.add.mr) * this.mult.mr
+}
+
+Champion.prototype.getAP = function(){
+    this.calculateItems();
+    return this.add.ap * this.mult.ap;
+}
+
+Champion.prototype.getCD = function(){
+    this.calculateItems();
+    var cd = 1;
+    this.cdr.forEach(function(cdr){
+        cd *= (1 - cdr);
+    })
+
+    if(cd < .6){cd = .6}
+    return cd;
+}
+
+Champion.prototype.getSpell = function(index){
+    var ret = this.base.spells[index];
+    ret.parsed = parse_spell(this, ret);
+    return ret;
+}
+
+Champion.prototype.getSpells = function(){
+    var arr = [];
+    for(var i = 0; i < 4; i++){
+        arr[i] = this.getSpell(i)
+    }
+    return arr
+}
+
+Champion.prototype.getItems = function(){
+    return this.state.items;
+}
+
+parse_spell = function(champ, spell){
+    var tt_vars = {};
+    var lvl = 0;
+    if($("#ability_level_" + spell.key)[0]!= undefined){
+        lvl = parseInt($("#ability_level_" + spell.key)[0].value);
+    }
+    var index = 0;
+    spell.effect.forEach(function(arr){
+        if(arr != null){
+            if(lvl == 0){
+                tt_vars["e" + index] = 0
+            }
+            else{
+                tt_vars["e" + index] = arr[lvl - 1]
+            }
+        }
+        index++;
+    })
+    if(spell.vars != undefined){
+        spell.vars.forEach(function(v){
+            var value;
+            switch(v.link){
+                case "spelldamage": value = champ.getAP(); break;
+                case "attackdamage":case "@dynamic.attackdamage": value = champ.getAD(); break;
+                case "bonusattackdamage": value = champ.getAD() - champ.getBaseAD(); break; /*FIXME Change to OO champion*/
+                case "@cooldownchampion": value = champ.getCD(); break;
+
+                default: alert(v.link)
+            }
+            if(lvl == 0){
+                value = 0
+            }
+            else if(v.coeff.length == 1){
+                value *= v.coeff[0]
+            }
+            else{
+                value *= v.coeff[lvl - 1]
+            }
+            tt_vars[v.key] = Math.round(value);
+        })
+    }
+
+    var output = spell.sanitizedTooltip
+
+    for(var key in tt_vars){
+        output = output.split("{{ " + key + " }}").join(tt_vars[key])
+    }
+
+    return output;
+}
--- champion_calculator.js
+++ champion_calculator.js
@@ -1,14 +1,13 @@
 /**
  * Created by Spencer on 7/11/15.
  */
-api_url="http://localhost:5000/"
+api_url="http://127.0.0.1:5000/"
 
 app = angular.module('build', []);
 
 $(function() {
-    url = "url(bg/" + getRandomInt(1, 2) + ".jpg)"
+    url = "url(bg/" + getRandomInt(1, 1) + ".jpg)"
     $("body").css("background-image", url);
-
 });
 
 function getRandomInt(min, max) {
@@ -23,26 +22,124 @@ function getUrlVars() {
     return vars;
 }
 
+var items = undefined;
+
 app.controller('CalculatorController', function($scope) {
     var calculator = this;
     var current_champion = undefined;
     var current_champion_api = undefined;
-    $.ajax({
-        method: "GET",
-        url: api_url + "api/lol/static-data/na/v1.2/champion/" + getUrlVars()["id"],
-        data: { "champData": "all" },
-        async: 'true'
-    }).done(
-        function(data){
-            current_champion_api = JSON.parse(data);
-            current_champion = new Champion(current_champion_api, null);
-            console.log(current_champion)
-            calculator.apply();
-            blur();
+    calculator.items = undefined;
+    var searched = undefined;
+
+    calculator.init = function(){
+        $.ajax({
+            method: "GET",
+            url: api_url + "api/lol/static-data/na/v1.2/champion/" + getUrlVars()["id"],
+            data: {"champData": "all"},
+            async: 'true'
+        }).done(
+            function (data) {
+                current_champion_api = JSON.parse(data);
+                current_champion = new Champion(current_champion_api, null);
+                console.log(current_champion)
+                calculator.apply();
+                blur();
+            }
+        ).fail(function (error) {
+                console.log(error)
+            });
+
+        $.ajax({
+            method: "GET",
+            url: api_url + "api/lol/static-data/na/v1.2/item",
+            data: {"itemListData": "all"},
+            async: 'true'
+        }).done(
+            function (data) {
+                calculator.items = JSON.parse(data);
+                items = calculator.items
+                console.log(calculator.items);
+                searched = searchItems("")
+                calculator.apply();
+            }
+        ).fail(function (error) {
+                console.log(error)
+            });
+    }
+
+    calculator.showSearch = function(){
+        return searched != undefined;
+    }
+
+    calculator.getItemSearchRows = function(){
+        return Math.ceil(searched.length / 3);
+    }
+
+    calculator.getItemSearchRow = function(row){
+        var end = row * 3 + 3;
+        if(end > searched.length){
+            end = searched.length
         }
-    ).fail(function(error){
-            console.log(error)
-        });
+
+        return searched.slice(row * 3, end)
+    }
+
+    calculator.update_search = function(){
+        searched = searchItems($("#item_search")[0].value)
+    }
+
+    var searchItems = function(term){
+        if(calculator.items === undefined){
+            return;
+        }
+        var out = [];
+        t = normalize(term)
+        for(var key in calculator.items.data) {
+            var item = calculator.items.data[key]
+            if (normalize(item.name).includes(t)) {
+                pushItem(item, out)
+                continue;
+            }
+            if(item.colloq != undefined){
+                if (normalize(item.colloq).includes(t)) {
+                    pushItem(item, out)
+                    continue;
+                }
+            }
+            else {
+                if(item == undefined){
+                    continue;
+                }
+                if(item.tags == undefined){
+                    continue;
+                }
+                if(item.tags.length == 0){
+                    continue;
+                }
+                item.tags.some(function (tag) {
+                    if (t === normalize(tag)) {
+                        pushItem(item, out)
+                        return true;
+                    }
+                    return false;
+                })
+            }
+        }
+        return out;
+    };
+
+    var pushItem = function(item, arr){
+        if($.inArray('Trinket', item.tags) > -1){
+            return;
+        }
+        arr.push(item.id);
+    }
+
+    var normalize = function(str){
+        out = str.split(" ").join();
+        out = out.toLowerCase()
+        return out;
+    }
 
     calculator.should_show = function(){
         return current_champion != undefined
@@ -62,6 +159,23 @@ app.controller('CalculatorController', function($scope) {
         return ability_levels;
     }
 
+    calculator.active_item = -1
+
+    calculator.item_clicked = function(event){
+        var src = event.srcElement || event.target
+        clicked = parseInt(src.id.substr(src.id.length - 1))
+        if(calculator.active_item != clicked){
+            calculator.active_item = clicked;
+        }
+        else{
+            calculator.active_item = -1;
+        }
+    }
+
+    calculator.set_level = function(){
+        current_champion.level = parseInt($("#champion_level")[0].value)
+    }
+
     calculator.apply = function(){
         $scope.$apply();
 
@@ -71,15 +185,31 @@ app.controller('CalculatorController', function($scope) {
         icon.height(text.height());
     }
 
+    calculator.should_show_item_popup = function(){
+        return calculator.active_item != -1
+    }
+
     var blur = function(){
         $(".blur").blurjs({
+            source: 'body',
             overlay: 'rgba(164, 164, 164, .5)',
+            cache: true,
             radius: 20
-        });
+        })
+
     }
 
     calculator.getImage = function(image){
         return "https://ddragon.leagueoflegends.com/cdn/5.12.1/img/" + image.group + "/" + image.full;
+    }
+
+    calculator.item_click = function(item){
+        if(item != undefined){
+            if(current_champion != undefined){
+                current_champion.state.items[calculator.active_item] = item;
+            }
+        }
+        calculator.active_item = -1
     }
 });
 
